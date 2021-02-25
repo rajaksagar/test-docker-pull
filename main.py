d325e65d558b4247af56bbb94f04fe19109cf1d1
rajaksagar <67678573+rajaksagar@users.noreply.github.com>
2021-02-26 04:26:12 +0530

    dsdsd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-print("Testing python environment: ")
+print("Testing python environment change: ")
 print(os.getenv('PYTHON_ENV'))
 print(os.getenv('NODE_ENV'))
 print(os.getenv('Steps'))
